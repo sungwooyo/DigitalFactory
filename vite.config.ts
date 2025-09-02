2c030d0bc84762e82f4a970ef76f8b580d76273f
sungwooy <sungwooyo@gmail.com>
2025-09-02 18:45:21 +0900

    ci: add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 
-const base = process.env.BASE_PATH || '/'
+const base = '/DigitalFactory/'; // 고정
 const isPreview = process.env.IS_PREVIEW  ? true : false;
 // https://vite.dev/config/
 export default defineConfig({
@@ -66,10 +66,7 @@ export default defineConfig({
     }),
   ],
   base,
-  build: {
-    sourcemap: true,
-    outDir: 'out',
-  },
+  build: { sourcemap: true, outDir: 'dist' }, // dist로 변경
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
